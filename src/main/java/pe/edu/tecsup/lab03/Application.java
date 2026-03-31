c9096c811a853a34b4ebeea5c84fe7fff295d8a2
Jaime <jgomezm@tecsup.edu.pe>
2026-03-31 17:20:28 -0500

    update

--- src/main/java/pe/edu/tecsup/lab03/Application.java
+++ src/main/java/pe/edu/tecsup/lab03/Application.java
@@ -1,4 +1,11 @@
 package pe.edu.tecsup.lab03;
 
 public class Application {
+
+    public static void main(String[] args) {
+        // TO DO
+        int jaime = 10;
+        int gomez = 11;
+    }
+
 }
